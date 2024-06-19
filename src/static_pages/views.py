d28b898c19e44cb43f5cdc0cc70d33c803e00c7f
Arleking <Abdukarim600@gmail.com>
2024-06-19 16:53:14 +0500

    update main page

--- src/static_pages/views.py
+++ src/static_pages/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from .models import index_page, openInfo
-from ceoInfo.models import CeoDatas
+from news.models import Post, PostAttachment
 # Create your views here.
 
 def index(request):
     content = index_page.objects.order_by('-id').first()
-    ceo_datas = CeoDatas.objects.order_by('id').first()
-    return render(request, 'MainPage/index.html', {'content': content, "ceo": ceo_datas})
+    posts = Post.objects.order_by('-date')
+    post_list = []
+    for post in posts:
+        original_content = post.content
+        post.content = ' '.join(original_content.split()[:40])
+        # Если содержимое было сокращено, добавляем многоточие в конец строки
+        if original_content != post.content:
+            post.content += ' ...'
+        att = PostAttachment.objects.filter(post_id = post.pk).first()
+        post_obj = {
+            'pk': post.pk if post else None,
+            'author': post.author if post else None,
+            'title': post.title if post else None,
+            'date': post.date if post else None,
+            'content': post.content if post else None,
+            'att': att if att else None,
+        }
+        post_list.append(post_obj)
+    return render(request, 'MainPage/index.html', {'content': content, "news": post_list})
 
 def site_map(request):
     return render(request, 'MainPage/site_map.html')
