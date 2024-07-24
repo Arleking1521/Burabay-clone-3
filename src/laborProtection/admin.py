acae95d7dae23d07b51f45c2aafb4179a29b2991
Arleking <Abdukarim600@gmail.com>
2024-07-24 17:36:54 +0500

    update admin.py in LaborProtection

--- src/laborProtection/admin.py
+++ src/laborProtection/admin.py
@@ -5,12 +5,12 @@ from django.utils.translation import gettext_lazy as _
 # Register your models here.
 class CustomLaborProtectionAdmin(TranslationAdmin):
     fieldsets = (
-        (_('Информация на русском'), {'fields': ( 'title_ru', ' content_ru',)}),
+        (_('Информация на русском'), {'fields': ( 'title_ru', 'content_ru',)}),
         (_('Информация на казахском'), {'fields': ('title_kk', 'content_kk',)}),
         (_('Информация на английском'), {'fields': ('title_en', 'content_en',)}),
     )
     add_fieldsets = (
-        (_('Информация на русском'), {'fields': ( 'title_ru', ' content_ru',)}),
+        (_('Информация на русском'), {'fields': ( 'title_ru', 'content_ru',)}),
         (_('Информация на казахском'), {'fields': ('title_kk', 'content_kk',)}),
         (_('Информация на английском'), {'fields': ('title_en', 'content_en',)}),
     )
